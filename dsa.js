4d151677d880f01c136e5163723c08a5fab621fb
shm_usuf <shmusuf@gmail.com>
2023-01-15 21:33:21 +0600

    first commit

---
+++ dsa.js
@@ -0,0 +1,18 @@
+/* ALgorithm */
+/******************/
+/* An Algorithm is a set of instructions or steps for solving problem */
+
+/* Why you should learn algorithm */
+/* 
+1. critical thinking and abstruction
+2. problem solving
+3. computer science
+4. career advancment
+5. understanding new tech
+
+*/
+
+/* time complexcity, space complexcity */
+/* time complexcity is a way to analyze the total time or performance of an algorithm */
+
+/* space complexcity is a way to analyze the total space or memory need for an algorithm */
