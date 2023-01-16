d252c8031931e6ce89af8f2cbb59f19ba0b2f120
shm_usuf <shmusuf@gmail.com>
2023-01-16 20:41:14 +0600

    fibonacci added

--- dsa.js
+++ dsa.js
@@ -25,4 +25,50 @@
 3. big theta notation
  */
 
-/*symbol of big o notation: O()  */
+/*symbol of big o notation: O(n)  */
+
+//if O(1), constant / best time or space complexcity
+
+//2nd best, O(log n)
+//3rd best, O(n) which is called linear time or space complexcity..
+
+//O(n log n)=> linear logarithm
+//O(n^x) => polynomial time, where x is a power
+
+//O(c^n)=> exponential time & space complexcity
+//O(n!)=> factorial
+
+// given an array, find the largest element
+
+function largestElement(arr) {
+  let largeEl = arr[0];
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] > largeEl) {
+      largeEl = arr[i];
+    }
+  }
+
+  return largeEl;
+}
+
+console.log(largestElement([5, 33, 2, 22, 25]));
+/* 
+el=5, run =2
+el = 100, run = 100
+el = n, run = n
+O(n)==>linear complexcity
+*/
+
+//given an array, find the first element
+
+function findFirstEl(arr) {
+  return arr.at(0);
+}
+
+console.log(findFirstEl([10, 15, 20, 25, 30]));
+
+/* 
+el = 5, run = 1
+el = n, run = 1
+O(1) == constant
+*/
