dddd749364a99d7850992722ad8d6f1cb0ed6f8c
shm_usuf <shmusuf@gmail.com>
2023-01-16 08:43:58 +0600

    asymtotic notation

--- dsa.js
+++ dsa.js
@@ -4,11 +4,11 @@
 
 /* Why you should learn algorithm */
 /* 
-1. critical thinking and abstruction
+1. critical thinking and abstruction(something hidden from general user)
 2. problem solving
 3. computer science
 4. career advancment
-5. understanding new tech
+5. understanding new technology
 
 */
 
@@ -16,3 +16,13 @@
 /* time complexcity is a way to analyze the total time or performance of an algorithm */
 
 /* space complexcity is a way to analyze the total space or memory need for an algorithm */
+
+/* asymptotic notation : this is a mathematical method. by this method we can describe or calculate time complexcity and space complexcity  */
+
+/* asymtotic notation are mainly three types: 
+1. big o notation
+2. big omega notation
+3. big theta notation
+ */
+
+/*symbol of big o notation: O()  */
